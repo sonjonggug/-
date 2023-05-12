0c4a66bb5e70ce3baf9c8f0fc9fb01629b259bd5
손종국 <kyky7852@naver.com>
2023-05-12 16:49:03 +0900

    InputStream , OutputStream 추가

--- src/run/ClientSocketTest.java
+++ src/run/ClientSocketTest.java
@@ -1,8 +1,10 @@
 package run;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -12,14 +14,31 @@ import java.net.Socket;
 public class ClientSocketTest {
 	
 	
-		public static void main(String[] args) {
+		public static void main(String[] args) throws IOException {
 				
+			Socket socket = null;
+			
 			try {
-				Socket socket = new Socket("localhost", 9500);
+				socket = new Socket("localhost", 9500);
 				System.out.println("연결성공");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		
+				//클라이언트 -> 소켓
+				String msg= "안녕하세요!";
+				PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+				writer.println(msg);  //서버로 데이터를 전송한다. 
+				writer.flush();   //버퍼 안에 있는 값들을 전부 비워준다. 
+				System.out.println("데이터 전송 완료!");
+				
+				//소켓 -> 클라이언트
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String line = reader.readLine();
+				System.out.println("데이터 받기 성공! :"+line);  //서버와 통신이 완료되어 "안녕하세요"라는 값을 가지고 온다.
+				
+				writer.close();
+				reader.close();
+						
 		}
 }
--- src/run/ServerSocketTest.java
+++ src/run/ServerSocketTest.java
@@ -1,13 +1,18 @@
 package run;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * 서버 소켓은 말그대로 서버프로그램에서만 사용되는 소켓이다. 
- * 서버소켓은 클라이언트로부터 연결요청이 오기를 기다렸다가 연결 요청이 오면 클라이언트와 연결을 맺고 다른 소켓을 만드는 일을 한다. 
+ * 소켓(socket)은 프로세스 간 통신에 사용되는 양쪽 끝 단을 의미한다.
+ * 쉽게 말해서 프로그램은 다른컴퓨터에 있는 프로그램과 통신하기 위해 바로 이 소켓을 이용하는데,
+ * 프로그램이 통신하기 위해 소프트웨어로 작성된 통신의 접속점이라고 할 수 있다. 
  */
 public class ServerSocketTest {
 	
@@ -16,8 +21,10 @@ public class ServerSocketTest {
 			
 			try {
 				
+				// 서버소켓은 클라이언트로부터 연결요청이 오기를 기다렸다가 연결 요청이 오면 클라이언트와 연결을 맺고 다른 소켓을 만드는 일을 한다.
 				ServerSocket serverSocket = new ServerSocket(9500); // 포트번호를 9500번으로 지정
-										
+				
+				// while문으로 돌려주어야지 계속 접속을 받을수 있다.
 				while (true) {
 					System.out.println("연결을 기다리는 중...");
 					
@@ -28,6 +35,16 @@ public class ServerSocketTest {
 					InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
 					
 					System.out.println("연결 수락됨" + isa.getHostName());
+					
+					// 소켓 -> 서버
+					BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));	// 데이터를 읽어옴
+					String line = reader.readLine(); 
+					
+					// 서버 -> 소켓
+					PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream())); // 소켓으로 데이터를 바깥으로 보냄
+
+					writer.println(line);
+					writer.flush();
 				}
 				
 			} catch (IOException e) {
